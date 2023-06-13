6d5a72bd9ae09080720bebeff02d54bea2c158b1
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-13 23:02:09 +0530

    b3

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function addRecord() {
     let inputMessageRef = document.querySelector("#messageId");
     let message = inputMessageRef.value;
   
-    let uri = `http://127.0.0.1:4000/addrecord?message=${message}`;
+    let uri = `http://localhost:4000/addrecord?message=${message}`;
   
     // connect the backend, ajax
     // magic, star, superstar
@@ -10,4 +10,24 @@ function addRecord() {
   
     // clear the box
     inputMessageRef.value = "";
+  
+    // refresh the page
+    readOrFindAllRecord();
+  }
+  
+  async function readOrFindAllRecord() {
+    let uri = "http://localhost:4000/findAll";
+  
+    let res = await fetch(uri);
+    let list = await res.json();
+    console.log(list);
+  
+    // iternateion
+    for (let item of list) {
+      let parentRef = document.querySelector("#parent");
+      let existingValue = parentRef.innerHTML;
+      let newValue = `<h1>${item.message}</h1>`;
+  
+      parentRef.innerHTML = newValue + existingValue;
+    }
   }
