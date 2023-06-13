d7c406f341621598a1401bc9a2f97365214bc8fd
Rishiv Yadav <rishavyadav51@gmail.com>
2023-06-13 22:58:16 +0530

    back end ajax

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 function addRecord() {
-    let uri = `http://127.0.0.1:4000/addrecord?message=hiiiiiiiii`;
+    let inputMessageRef = document.querySelector("#messageId");
+    let message = inputMessageRef.value;
+  
+    let uri = `http://127.0.0.1:4000/addrecord?message=${message}`;
   
     // connect the backend, ajax
+    // magic, star, superstar
     fetch(uri);
+  
+    // clear the box
+    inputMessageRef.value = "";
   }
